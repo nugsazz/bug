8a9d63339c56ad7dbfe8ee409844944c1fc4f92f
Zekai10 <32733245+Zekai10@users.noreply.github.com>
2017-10-15 16:40:06 +0700

    Add files via upload

--- zeusbot.py
+++ zeusbot.py
@@ -1508,6 +1508,7 @@ def bot(op):
 							kc.leaveGroup(msg.to)
 						except:
 							pass
+							cl.sendText(msg.to"Dadah")
             elif msg.text in ["Bye 1"]:
 				if msg.from_ in admin:
 					if msg.toType == 2:
